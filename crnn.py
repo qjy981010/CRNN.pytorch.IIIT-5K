d369b8fc332fb65f0458a43b562708d43c2c9343
qjy981010 <1114943038@qq.com>
2018-04-13 20:58:32 +0800

    add init

--- crnn.py
+++ crnn.py
@@ -57,3 +57,14 @@ class CRNN(nn.Module):
             if (self.pool_struct[i]):
                 cnn_layers.append(nn.MaxPool2d(self.pool_struct[i]))
         return nn.Sequential(*cnn_layers)
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
